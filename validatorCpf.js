de6675d2cceb6130a28516967ee3eaaa9f3e46c0
joao <joaodeus400@gmail.com>
2023-09-14 18:22:09 -0300

    cpf teste

--- validatorCpf.js
+++ validatorCpf.js
@@ -1,17 +1,35 @@
 function TesteCPF(cpf) {
-    let soma
-    let resto
+    cpf = cpf.replace(/[^\d]+/g,'');	
+    if (cpf.length != 11|| cpf === "00000000000" || cpf == "11111111111" || 
+    cpf == "22222222222" || 
+    cpf == "33333333333" || 
+    cpf == "44444444444" || 
+    cpf == "55555555555" || 
+    cpf == "66666666666" || 
+    cpf == "77777777777" || 
+    cpf == "88888888888" || 
+    cpf == "99999999999") 
+    // console.log('invalid')
+    
+    // valida 1 digito
     soma = 0
-        if (cpf === "00000000000") return false
+        for (i=0; i<=9; i++) soma =+ parseInt(cpf.charAt(i)) * (10 - i);
+        resto = 11 - (soma % 11)
+         
+        if (resto == 10 || resto == 11) resto = 0
+        if (resto != parseInt(cpf.charAt(9)))
+        console.log('invalid')
+    // valida 2 digito
+    soma = 0
+        for (i=0;i<=10; i++) soma += parseInt(cpf.charAt(i)) * (11 - i);
+        resto = 11 - (soma % 11)
+        if (resto == 10 || resto == 11)
+            resto = 0
+        if (resto != parseInt(cpf.charAt(10)))
+        console.log('invalid')
 
-        for (i=1; i<=9; i++) soma = soma + parseInt(cpf.substring(i-1,i)) * (11 - i);
-        resto = (soma * 10)% 11
-        const resultado = soma + resto
+    return true
 
-        console.log(resultado)
-       
-        if ((resto == 10) || (resto == 11)) resto = 0
-        if (resto != parseInt(cpf.substring(9, 10)) ) return false
 
 }
 
