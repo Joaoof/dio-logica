541abf434f0fcaf4ef94190fe478756dbed09fc8
Joaoof <joaodeus400@gmail.com>
2023-09-15 09:22:34 -0300

    matrizes

---
+++ colecao/matrizes.js
@@ -0,0 +1,12 @@
+let time = [
+    [
+        "Flamengo", "Cruzeiro", "Botafogo"
+    ],
+    [
+        "Grêmio", "Fluminese", "Vasco da Gama"
+    ]
+]
+
+
+console.log('O melhor time do mundo é o:',time[0][0]) // o primeiro "[0]" seleciona a primeira linha, logo em seguida pega a posição do elemento dentro da mesma
+console.log('foi de comes e bebes, foi jogar no:',time[1][2])
