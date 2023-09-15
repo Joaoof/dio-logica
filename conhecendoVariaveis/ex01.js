890651524401e08a4c7babf50bb734c9b349bde0
Joaoof <joaodeus400@gmail.com>
2023-09-15 08:44:49 -0300

    logica

--- conhecendoVariaveis/ex01.js
+++ conhecendoVariaveis/ex01.js
@@ -1,7 +1,7 @@
 let nome = "Maria Socorro"
 let idade = 80
 let telefone = 992006162
-let endereço = 'Rua 05 Q 68 LT 09'
+let endereco = 'Rua 05 Q 68 LT 09'
 
 console.log(`Olá! o nome da minha vó é: ${nome}. Ela possui a idade de ${idade} anos, seu telefone para contato é ${telefone}`)
 console.log('Por favor digite o seu cpf e endereço de sua vó: ')
