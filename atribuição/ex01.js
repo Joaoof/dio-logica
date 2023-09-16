f0ce6bf17b12680ae9ab356ba25dd2d6866a3f08
Joaoof <joaodeus400@gmail.com>
2023-09-16 12:35:02 -0300

    atribuição

---
+++ atribuição/ex01.js
@@ -0,0 +1,14 @@
+let preco = 20
+preco += 4 
+
+console.log(preco)
+
+let precoDois = 1500
+precoDois *= 5
+
+console.log('Esse será meu salário daqui 3 anos:', precoDois)
+
+let restoDaDivision = 200
+restoDaDivision %= 4 // 50 * 4 = 200, logo não sobra nada para 200 - 200 = 0
+
+console.log(restoDaDivision)
