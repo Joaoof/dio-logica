a549dc6dba4fe13196b89a1d897a7697f7d5b923
Joaoof <joaodeus400@gmail.com>
2023-09-18 08:36:41 -0300

    operadores de comparação

--- operadores/app.js
+++ operadores/app.js
@@ -1,53 +1,18 @@
-// let precoProdutoNormal = 250
-// let precoTaxado = 0.92  // o processo de taxa funciona dividindo 92 por 100 92/100 = 0.92
-// let precoParcelado = 2
-
-// function TaxaDoAmor() {
-//     const precoProdutoNormal = parseFloat(document.getElementById('precoProduto'))
-//     const precoTaxado = parseFloat(document.getElementById('taxaJuros').value)
-
-//     if (isNaN(precoProdutoNormal) || isNaN(precoTaxado)) {
-//         alert('Por favor, insira valores válidos.')
-//         return
-//     }
-
-//     let juros = precoProdutoNormal * precoTaxado 
-//     let valorTotalFazOL = precoProdutoNormal + juros
-
-//     const resultadoDiv = document.getElementById('resultado');
-//     resultadoDiv.innerHTML = `
-//         <p>Valor do produto: R$ ${precoProdutoNormal.toFixed(2)}</p>
-//         <p>Juros: R$ ${juros.toFixed(2)}</p>
-//         <p>Valor total com juros: R$ ${valorTotalFazOL.toFixed(2)}</p>
-//     `;
-    
-//     if ( precoProdutoNormal != precoTaxado ) {
-//         console.log('Seu valor foi taxado');
-//         console.log('Você pode parcelar caso deseje:', valorTotalFazOL / precoParcelado, 'reais' )
-//     } else {
-//         console.log('Você não foi taxado')
-//    }
-//     console.log('total de juros, faz o L:', valorTotalFazOL, 'reais')
-// }
-
-// TaxaDoAmor(precoProdutoNormal, precoTaxado)
-
-// depois melhora essa fuction
-
-
-
+let precoProdutoNormal = 250
+let precoTaxado = 0.92  // o processo de taxa funciona dividindo 92 por 100 92/100 = 0.92
+let precoParcelado = 2
 
 function TaxaDoAmor() {
-    const precoProdutoNormal = parseFloat(document.getElementById('precoProdutoNormal').value);
-    const taxaJurosDoAmor = parseFloat(document.getElementById('taxaJurosDoAmor').value);
+    const precoProdutoNormal = parseFloat(document.getElementById('precoProduto'))
+    const precoTaxado = parseFloat(document.getElementById('taxaJuros').value)
 
-    if (isNaN(precoProdutoNormal) || isNaN(taxaJurosDoAmor)) {
-        alert('Por favor, insira valores válidos.');
-        return;
+    if (isNaN(precoProdutoNormal) || isNaN(precoTaxado)) {
+        alert('Por favor, insira valores válidos.')
+        return
     }
 
-    const juros = precoProdutoNormal * taxaJurosDoAmor;
-    const valorTotalFazOL = precoProdutoNormal + taxaJurosDoAmor;
+    let juros = precoProdutoNormal * precoTaxado 
+    let valorTotalFazOL = precoProdutoNormal + juros
 
     const resultadoDiv = document.getElementById('resultado');
     resultadoDiv.innerHTML = `
@@ -55,5 +20,19 @@ function TaxaDoAmor() {
         <p>Juros: R$ ${juros.toFixed(2)}</p>
         <p>Valor total com juros: R$ ${valorTotalFazOL.toFixed(2)}</p>
     `;
+    
+    if ( precoProdutoNormal != precoTaxado ) {
+        console.log('Seu valor foi taxado');
+        console.log('Você pode parcelar caso deseje:', valorTotalFazOL / precoParcelado, 'reais' )
+    } else {
+        console.log('Você não foi taxado')
+   }
+    console.log('total de juros, faz o L:', valorTotalFazOL, 'reais')
 }
+
+TaxaDoAmor(precoProdutoNormal, precoTaxado)
+
+
+
+
 
---
+++ operadores/ex01.js
@@ -0,0 +1,8 @@
+let phone = "Apple"
+let phone2 = "Android"
+
+if (phone !== "Apple") {
+    console.log("Muito caro")
+} else {
+    console.log("Passou, teste")
+}
