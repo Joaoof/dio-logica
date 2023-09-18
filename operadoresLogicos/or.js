91b93fa8cd9bc047035e1790a634e7262204d510
Joaoof <joaodeus400@gmail.com>
2023-09-18 08:57:47 -0300

    or

---
+++ operadoresLogicos/or.js
@@ -0,0 +1,12 @@
+let tempo = "sol"
+let item = "guarda chuva"
+
+let podeSair = (tempo !== "chuva") || (item === "guarda chuva")
+console.log(podeSair)
+
+let encontro = "2 pessoas"
+let local = "restaunte"
+
+let vamosSair = (encontro == "2 pessoas" || local == restaurante)
+
+console.log(vamosSair)
