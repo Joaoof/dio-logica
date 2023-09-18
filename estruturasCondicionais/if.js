197c150dc450c91313f62f81fb18535161d85183
Joaoof <joaodeus400@gmail.com>
2023-09-18 09:59:32 -0300

    estruturas

---
+++ estruturasCondicionais/if.js
@@ -0,0 +1,9 @@
+let nivelDeFome = 3
+
+if(nivelDeFome === 1) {
+    console.log("pouca fome")
+} else if (nivelDeFome == 2) {
+    console.log("fome mediana")
+} else {
+    console.log("voce foi de base");
+}  
