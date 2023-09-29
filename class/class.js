a7846456ed022ae820abe7f4c8188b62a241b3ce
Joaoof <joaodeus400@gmail.com>
2023-09-29 08:28:53 -0300

    "class"

--- class/class.js
+++ class/class.js
@@ -3,7 +3,14 @@ class formaDeBolo {
         this.saborDaMassa =  saborDaMassa
         this.saborRecheio =  saborRecheio
     }
+
+    escrever() {
+        console.log(`um delicioso bolo de ${this.saborDaMassa} com recheio de ${this.saborRecheio}`)
+    }
 }
 
 let boloFesta = new formaDeBolo("massa de chocolate", "recheio de nutella")
-console.log(boloFesta);
+let boloPremium = new formaDeBolo("baunilha", "coco")
+
+boloFesta.escrever()
+boloPremium.escrever()
